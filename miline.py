4af434a7a48a4077c013028766737f9be91d46cd
shehabalaa <shehabalaa97@gmail.com>
2018-12-14 17:06:25 +0200

    Change print message

--- miline.py
+++ miline.py
@@ -58,7 +58,7 @@ def test_equations(eqs,variables):
 def read_input():
     n = int(input("Enter number of equations: "))
     print("Enter equations as python format ex: -2*x**3 + exp(x)-20*x + 8.5")
-    print("Note That y0 is the name of variable 2 equations ex:\n 1-> dy0/dx = 3*x*y1 + 2y0\n 2-> dy1/dx = x + y1 + y0\n")
+    print("Note That y0 is the name of variable 2 equations ex:\n 1-> dy0/dx = 3*x*y1 + 2*y0\n 2-> dy1/dx = x + y1 + y0\n")
     eqs = [input("eq{0}: dy{0}/dx = ".format(i)) for i in range(n)]
     print("\nIntial values for variables")
     h = float(input("step size h = "))
